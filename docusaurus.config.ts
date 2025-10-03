6e1285d7e71b2ff0d6c5f6a62cdfa88f46080d1d
alexa <alex@targoninc.com>
2025-10-03 22:32:33 +0200

    fix banner

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -59,7 +59,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/banner.png',
     colorMode: {
       respectPrefersColorScheme: true,
     },
